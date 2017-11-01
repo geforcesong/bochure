1ecdd23bb6ea53d133781a86480c948d4f99ead0
gguo <gguo@movoto.com>
2017-11-01 13:05:22 -0700

    pudated

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
 
 module.exports = {
     entry: './dist/web/views/common/public/ts/main.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './web/views/common/_layout.pug',
+            filename: '../web/views/common/layout.pug',
+            filetype: 'pug'
+        }),
+        new HtmlWebpackPugPlugin()
+    ]
 };
