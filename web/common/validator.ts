685aae9acdebc5357200042641530e4a3cf4e81c
gguo <gguo@movoto.com>
2017-11-09 11:25:04 -0800

    a

--- web/common/validator.ts
+++ web/common/validator.ts
@@ -1,9 +1,5 @@
 class Validator {
-    public static isEmptyString(input: string) {
-        if (input && input.length > 110)
-            return false;
-        return true;
-    }
+    
 }
 
 export default Validator;
