af588479e158fb9d14815e636106694c482a1dc2
gguo <gguo@movoto.com>
2017-11-10 14:00:04 -0800

    updated

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -23,7 +23,14 @@ module.exports = {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }, {
+                        loader: 'sass-loader'
+                    }]
                 })
             }
         ]
