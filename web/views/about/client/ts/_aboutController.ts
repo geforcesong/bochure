7c9a200fed5bcd2d8048bc96815c7ed7723c1e13
gguo <gguo@movoto.com>
2017-11-15 16:47:21 -0800

    added vue

--- web/views/about/client/ts/_aboutController.ts
+++ web/views/about/client/ts/_aboutController.ts
@@ -1,8 +1,53 @@
 import * as _ from 'lodash';
+import Vue from 'vue';
 
 class AboutController {
+    app: Vue;
     constructor() {
-        console.log('this is about page');
+        Vue.component('todo-item', {
+            // todo-item 组件现在接受一个
+            // "prop"，类似于一个自定义属性
+            // 这个属性名为 todo。
+            props: ['todo'],
+            template: '<li>{{ todo.id + 1 }}-{{ todo.text }}<button v-on:click="$emit(\'remove\')">X</button></li>'
+        })
+        this.app = new Vue({
+            el: '#app',
+            data: {
+                message: 'Hello Vue!' + new Date().toLocaleString(),
+                seen: true,
+                groceryList: [
+                    { id: 0, text: 'Car' },
+                    { id: 1, text: 'Plane' },
+                    { id: 2, text: 'Tank' }
+                ],
+                classObj: {
+                    active: true,
+                    noerror: true,
+                    err: false,
+                    er: true
+                },
+                loginType: 'username'
+            },
+            computed: {
+                reversedMessage: function () {
+                    return this.message.split('').reverse().join('');
+                }
+            },
+            methods: {
+                reverseMessage: function () {
+                    this.message = this.message.split('').reverse().join('');
+                    let itm = {
+                        id: this.groceryList.length,
+                        text: `item${this.groceryList.length}`
+                    }
+                    this.groceryList.push(itm);
+                }
+            },
+            created: function () {
+                console.log('create app');
+            }
+        })
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,11 @@ module.exports = {
             }
         ]
     },
+    resolve: {
+        alias: {
+            vue$: 'vue/dist/vue.esm.js'
+        }
+    },
     plugins: [
         new ExtractTextPlugin('styles.css'),
         new HtmlWebpackPlugin({
