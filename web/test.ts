8e591c23e18f5e5c5d72b33dd3ab5bae436398e8
gguo <gguo@movoto.com>
2017-11-28 10:31:09 -0800

    updated categories

--- web/test.ts
+++ web/test.ts
@@ -3,8 +3,17 @@ import DB from './database/mongo/mongoManager';
 const db = new DB();
 
 async function name() {
-    const cdb = await db.connect();
-    console.log(cdb);
+    await db.connect();
+    let categories = await db.collection('categories').find({}).toArray();
+    if(categories && categories.length){
+        var bulk = db.collection('categories').initializeUnorderedBulkOp();
+        let i=1;
+        for(var item of categories[0].subcategories ){
+            bulk.insert({name: item.name, order: i++});
+        }
+        await bulk.execute();
+    }
+    await db.close();
 };
 
 name();
